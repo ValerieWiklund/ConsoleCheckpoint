77ef2052941542e9ac6ff41c71064a0bde08eee0
Valerie Wiklund <vkwiklund@gmail.com>
2019-10-09 17:23:47 -0600

    starting infrastructure

--- Project/Models/Room.cs
+++ Project/Models/Room.cs
@@ -3,11 +3,29 @@ using ConsoleAdventure.Project.Interfaces;
 
 namespace ConsoleAdventure.Project.Models
 {
-    public class Room : IRoom
+  public class Room : IRoom
+  {
+
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<Item> Items { get; set; }
+    public Dictionary<string, IRoom> Exits { get; set; }
+
+
+    public void AddExit(IRoom, room)
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public List<Item> Items { get; set; }
-        public Dictionary<string, IRoom> Exits { get; set; }
+      CollectionExtensions.TryAdd(room.Name, room)
     }
+
+
+
+    public Room(string name, string desc, List<Item> items, Dictionary<string, IRoom> exits)
+    {
+      Name = name;
+      Description = desc;
+      Items = new List<Item>();
+      Exits = new Dictionary<string, IRoom>();
+    }
+
+  }
 }
